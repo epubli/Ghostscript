dd4cd94414cb6325576f1c0f9edb557c05a441e3
Poschi <poschi@poschi.local>
2016-10-20 20:00:31 +0200

    Added new Ghostscript device "inkcov" for analyzing color information.

---
+++ src/Device/Inkcov.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the Ghostscript package
+ */
+
+namespace GravityMedia\Ghostscript\Device;
+
+use GravityMedia\Ghostscript\Ghostscript;
+use GravityMedia\Ghostscript\Process\Arguments as ProcessArguments;
+use GravityMedia\Ghostscript\Process\Arguments;
+use Symfony\Component\Process\ProcessBuilder;
+
+/**
+ * @package GravityMedia\Ghostscript\Devices
+ */
+class Inkcov extends AbstractDevice
+{
+    const POSTSCRIPT_COMMANDS = '';
+
+    /**
+     * @param Ghostscript $ghostscript
+     * @param ProcessArguments $arguments
+     */
+    public function __construct(Ghostscript $ghostscript, Arguments $arguments)
+    {
+        parent::__construct($ghostscript, $arguments->setArgument('-sDEVICE=inkcov'));
+    }
+}
